5d7695a5d10a8a633b70a5aa5328bd7f63911eee
Jacob Lurye <jlurye@college.harvard.edu>
2020-05-04 23:08:34 -0400

    Redirect from 404 to /

--- website/src/pages/404.jsx
+++ website/src/pages/404.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { navigate } from "gatsby";
 
-import Layout from "../components/Layout";
-import SEO from "../components/Seo";
+const NotFoundPage = () => {
+  if (typeof window !== "undefined") {
+    navigate("/");
+  }
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+  return null;
+};
 
 export default NotFoundPage;
